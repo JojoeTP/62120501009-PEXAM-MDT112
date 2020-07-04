c2e13c1cfcf8fb18600b1c31d39f2ac69837271f
Windwill <jojoeza10@gmail.com>
2020-07-04 14:16:20 +0700

    3. Complete

--- Final/Final.ino
+++ Final/Final.ino
@@ -1,11 +1,81 @@
+
+int motorPin1 = 8;// Blue   - 28BYJ48 pin 1
+int motorPin2 = 9;// Pink   - 28BYJ48 pin 2
+int motorPin3 = 10;// Yellow - 28BYJ48 pin 3
+int motorPin4 = 11;// Orange - 28BYJ48 pin 4
+int motorSpeed = 2;     //variable to set stepper speed
+
+void clockwise(){
+ digitalWrite(motorPin4, HIGH);
+  digitalWrite(motorPin3, LOW);
+  digitalWrite(motorPin2, LOW);
+  digitalWrite(motorPin1, LOW);
+  delay(motorSpeed);
+  // 2
+  digitalWrite(motorPin4, HIGH);
+  digitalWrite(motorPin3, HIGH);
+  digitalWrite(motorPin2, LOW);
+  digitalWrite(motorPin1, LOW);
+  delay (motorSpeed);
+  // 3
+  digitalWrite(motorPin4, LOW);
+  digitalWrite(motorPin3, HIGH);
+  digitalWrite(motorPin2, LOW);
+  digitalWrite(motorPin1, LOW);
+  delay(motorSpeed);
+  // 4
+  digitalWrite(motorPin4, LOW);
+  digitalWrite(motorPin3, HIGH);
+  digitalWrite(motorPin2, HIGH);
+  digitalWrite(motorPin1, LOW);
+  delay(motorSpeed);
+  // 5
+  digitalWrite(motorPin4, LOW);
+  digitalWrite(motorPin3, LOW);
+  digitalWrite(motorPin2, HIGH);
+  digitalWrite(motorPin1, LOW);
+  delay(motorSpeed);
+  // 6
+  digitalWrite(motorPin4, LOW);
+  digitalWrite(motorPin3, LOW);
+  digitalWrite(motorPin2, HIGH);
+  digitalWrite(motorPin1, HIGH);
+  delay (motorSpeed);
+  // 7
+  digitalWrite(motorPin4, LOW);
+  digitalWrite(motorPin3, LOW);
+  digitalWrite(motorPin2, LOW);
+  digitalWrite(motorPin1, HIGH);
+  delay(motorSpeed);
+  // 8
+  digitalWrite(motorPin4, HIGH);
+  digitalWrite(motorPin3, LOW);
+  digitalWrite(motorPin2, LOW);
+  digitalWrite(motorPin1, HIGH);
+  delay(motorSpeed);
+}
+
+
+
 void setup(){
     pinMode(8,OUTPUT);
     tone(8,900,600);
     Serial.begin(9600);
     Serial.println("Hello MDT");
+    pinMode(motorPin1, OUTPUT);
+    pinMode(motorPin2, OUTPUT);
+    pinMode(motorPin3, OUTPUT);
+    pinMode(motorPin4, OUTPUT);
+    for(int i = 0;i<=500;i++){
+        clockwise();
+    }
+    Serial.println("Step CW 360  Degrees");
+    delay(900);
     
 
 }
+
+
 
 void loop(){
     
